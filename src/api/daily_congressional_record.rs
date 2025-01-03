13ba3862cc48398f263604930378e0ec8043fe88
Wollaston <matt@wollaston.dev>
2025-01-02 21:34:36 -0500

    Update daily-congressional-record endpoint

--- src/api/daily_congressional_record.rs
+++ src/api/daily_congressional_record.rs
@@ -1,72 +1,13 @@
-use derive_builder::Builder;
-use http::Method;
-use std::borrow::Cow;
+#![allow(clippy::module_inception)]
 
-use crate::{endpoint::Endpoint, params::QueryParams};
-
-use super::Format;
-
+mod articles;
+mod daily_congressional_record;
+mod issue_number;
 mod volume_number;
 
-/// Represents the /daily-congressional-record endpoint.
-#[derive(Debug, Clone, Copy, Builder)]
-#[builder(setter(strip_option))]
-pub struct DailyCongressionalRecord {
-    #[builder(default)]
-    format: Format,
-    #[builder(default)]
-    offset: Option<u32>,
-    #[builder(default)]
-    limit: Option<u8>,
-}
-
-impl DailyCongressionalRecord {
-    pub fn builder() -> DailyCongressionalRecordBuilder {
-        DailyCongressionalRecordBuilder::default()
-    }
-}
-
-impl Endpoint for DailyCongressionalRecord {
-    fn method(&self) -> Method {
-        Method::GET
-    }
-
-    fn endpoint(&self) -> Cow<'static, str> {
-        "daily-congressional-record".to_string().into()
-    }
-
-    fn parameters(&self) -> QueryParams {
-        let mut params = QueryParams::default();
-
-        params.push("format", self.format);
-        params.push_opt("offset", self.offset);
-        params.push_opt("limit", self.limit);
-
-        params
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::{
-        api::daily_congressional_record::DailyCongressionalRecord, auth::Auth, cdg::Cdg,
-        query::Query,
-    };
-
-    #[test]
-    fn is_sufficient() {
-        DailyCongressionalRecord::builder().build().unwrap();
-    }
-
-    #[tokio::test]
-    async fn endpoint() {
-        dotenvy::dotenv().unwrap();
-
-        let auth = Auth::Token(dotenvy::var("CDG_API_KEY").unwrap());
-        let client = Cdg::new(auth).unwrap();
-
-        let endpoint = DailyCongressionalRecord::builder().build().unwrap();
-
-        let _res: serde_json::Value = endpoint.query(&client).await.unwrap();
-    }
-}
+pub use self::articles::{Articles, ArticlesBuilder, ArticlesBuilderError};
+pub use self::daily_congressional_record::{
+    DailyCongressionalRecord, DailyCongressionalRecordBuilder, DailyCongressionalRecordBuilderError,
+};
+pub use self::issue_number::{IssueNumber, IssueNumberBuilder, IssueNumberBuilderError};
+pub use self::volume_number::{VolumeNumber, VolumeNumberBuilder, VolumeNumberBuilderError};
